1df6f4edf49ae23b3cb34ddbc1d55c3ca17a1907
Mickey <makyahpaul0@gmail.com>
2021-11-05 11:18:57 -0400

    fix auto updater

--- modules/updater.py
+++ modules/updater.py
@@ -11,11 +11,11 @@ def check():
     set_title("Calani AIO | Checking For Updates | MickeyYe#0003")
     print(f"    [{cyan}>{reset}] Checking for updates")
     try:
-        v = repr(get("https://raw.githubusercontent.com/Mickey758/Calani-AIO/master/version").text.rstrip())
-        if v != "0.1.5-alpha":
-            return False
-        else:
+        v = get("https://raw.githubusercontent.com/Mickey758/Calani-AIO/master/version").text.rstrip()
+        if v != "0.1.5.1-alpha":
             return True
+        else:
+            return False
     except:
         print(f"    [{red}>{reset}] Could not connect to update server")
         sleep(2)
