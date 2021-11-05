db7e498bf4123f2f49fd0968c73f1d7786137aa2
Mickey <makyahpaul0@gmail.com>
2021-11-05 11:13:27 -0400

    fix autoupdater

--- modules/updater.py
+++ modules/updater.py
@@ -11,9 +11,9 @@ def check():
     set_title("Calani AIO | Checking For Updates | MickeyYe#0003")
     print(f"    [{cyan}>{reset}] Checking for updates")
     try:
-        v = get("https://raw.githubusercontent.com/Mickey758/Calani-AIO/master/version").text
+        v = repr(get("https://raw.githubusercontent.com/Mickey758/Calani-AIO/master/version").text.rstrip())
         if v != "0.1.5-alpha":
-            return True
+            return False
         else:
             return True
     except:
