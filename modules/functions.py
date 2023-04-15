f5da5195985baa2f19dba46b7117f65b37264e5c
MickeyYe <79796196+Mickey758@users.noreply.github.com>
2023-04-14 21:44:30 -0400

    bypass recaptcha v3 func

--- modules/functions.py
+++ modules/functions.py
@@ -12,6 +12,7 @@ from os import makedirs
 import win32gui,win32process,os
 from numerize.numerize import numerize
 from colored import fg
+from requests import Session
 import ctypes
 
 """Color variables"""
@@ -83,6 +84,24 @@ def ascii():
         
         
         """+reset)
+def bypass_recaptcha3(get_url:str,post_url:str,proxy:str={}):
+    from urllib.parse import urlparse
+    from urllib.parse import parse_qs
+    url_variables = parse_qs(urlparse(get_url).query)
+    with Session() as s:
+        s.proxies.update(proxy)
+
+        r = s.get(get_url,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"})
+        token_1 = r.text.split("type=\"hidden\" id=\"recaptcha-token\" value=\"")[1].split("\"")[0]
+        
+        data = f"v={url_variables['v'][0]}&reason=q&c={token_1}&k={url_variables['k'][0]}&co={url_variables['co'][0]}&hl=en&size=invisible"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36" ,
+            "referer": r.url ,
+            "Content-Type":"application/x-www-form-urlencoded" 
+        }
+        r = s.post(post_url,headers=headers,data=data)
+        return r.text.split("[\"rresp\",\"")[1].split("\"")[0]
 
 def get_time():
     """
